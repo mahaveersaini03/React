c0bff74cf0d17d9d4f607d3d9b826b6b792f2c8c
mahaveersaini03 <mahaveersaini03@gmail.com>
2023-11-06 23:49:10 +0530

    react frontend app commited

---
+++ react-frontend/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css";
+const Header = () => {
+  return (
+    <header >
+        <nav className='header-nav'>
+              <ul className='header-ul'>
+                <li>Home</li>
+                <li>Products</li>
+              </ul>
+        </nav>
+         </header>
+  )
+}
+
+export default Header
